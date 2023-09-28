a73ea1cb0211cd47a2c06a9f2723e13214b8b50a
phdevlpr <phdevlpr@gmail.com>
2023-09-28 15:58:15 -0300

    Tarefa Módulo 9 Ebac Java

--- md5/primeiraClasse.java
+++ md5/primeiraClasse.java
@@ -1,5 +1,3 @@
-package tarefasEbacCursoJava.md5;
-
 public class primeiraClasse {
     public static void main(String[] args) {
         System.out.print("Hello Paulo!");
---
+++ md9/tarefa_wrapper/TarefaModuloNove.java
@@ -0,0 +1,15 @@
+/**
+ * @author phtech
+ */
+ public class TarefaModuloNove {
+
+    public static void main(String[] args) {
+
+        int numInteiro = 20;
+        System.out.println(numInteiro);
+
+        Integer numInteger = (Integer) numInteiro;
+        System.out.println(numInteger);
+    }
+
+ }
